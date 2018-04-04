817967072439a379df196fce1cd1a7352826efe5
Desty NA <destynursyifaa@gmail.com>
2018-04-04 05:24:39 -0700

    Instance, buat atribut dan panggil method

--- src/praktek3/SapiAksi.java
+++ src/praktek3/SapiAksi.java
@@ -1,6 +1,11 @@
 package praktek3;
 public class SapiAksi {
     public static void main(String[] args) {
+        Sapi s = new Sapi();
+        s.Jenis="Sapi Potong";
+        s.Warna="Putih Hitam";
+        s.Harga=4000000;
         
+        s.cetakInfo();
     }
 }
