20bb8ccbcf632855c9f8caf38a98dd9cf9f931b6
Desty NA <destynursyifaa@gmail.com>
2018-04-04 06:03:49 -0700

    Attribute dan Method

--- src/praktek3/SapiHereford.java
+++ src/praktek3/SapiHereford.java
@@ -1,5 +1,10 @@
 package praktek3;
 
 public class SapiHereford extends Sapi{
+    private String Jenis_SapiHereford;
+    
+    void throttle(){
+        System.out.println("emMMMoooooooo");
+    }
     
 }
