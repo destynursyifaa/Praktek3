5d73b808a5fff0416ce1a259038c2ce754b4e0d6
Desty NA <destynursyifaa@gmail.com>
2018-04-04 04:52:35 -0700

    Buat method cetakinfo()

--- src/praktek3/Sapi.java
+++ src/praktek3/Sapi.java
@@ -4,4 +4,8 @@ public class Sapi {
     String Jenis;
     String Warna;
     Integer Harga;
+    
+    void cetakInfo(){
+        System.out.println("Jenis \t: "+Jenis+"\nWarna \t: "+Warna+"\nHarga \t: "+Harga);
+    }
 }
