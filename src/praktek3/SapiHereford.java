511b984053e02c6348d5e7c545a1f566d8c1c970
Desty NA <destynursyifaa@gmail.com>
2018-04-04 06:05:15 -0700

    Getter and Setter

--- src/praktek3/SapiHereford.java
+++ src/praktek3/SapiHereford.java
@@ -6,5 +6,15 @@ public class SapiHereford extends Sapi{
     void throttle(){
         System.out.println("emMMMoooooooo");
     }
+
+    public String getJenis_SapiHereford() {
+        return Jenis_SapiHereford;
+    }
+
+    public void setJenis_SapiHereford(String Jenis_SapiHereford) {
+        this.Jenis_SapiHereford = Jenis_SapiHereford;
+    }
+    
+    
     
 }
